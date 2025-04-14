9822dc9dd3771cc72fa9ac50b1cb1067232c7e3b
Naumov <naumov.a.n@mail.ru>
2025-04-14 20:43:07 +0700

    create class Scene

--- models/headers/Scene.hpp
+++ models/headers/Scene.hpp
@@ -1,10 +1,27 @@
 #pragma once
 
+#include <iostream>
+#include <memory>
+#include <vector>
+#include <PoligonalModel.hpp>
+#include <Flash.hpp>
+
 class Scene
 {
 private:
-    
+    int m_id;
+    std::vector<std::shared_ptr<PoligonalModel>> m_poligonalModels;
+    std::vector<std::shared_ptr<Flash>> m_flashs;
 public:
     Scene() = default;
     ~Scene() = default;
+
+    void addPoligonalModel(std::shared_ptr<PoligonalModel> model);
+    void addFlash(std::shared_ptr<Flash> flash);
+
+    std::shared_ptr<PoligonalModel> getPoligonalModel(int index);
+    std::shared_ptr<Flash> getFlash(int index);
+
+    void removePoligonalModel(int index);
+    void removeFlash(int index);
 };
---
+++ models/src/Scene.cpp
@@ -0,0 +1,35 @@
+#include "Scene.hpp"
+#include "Camera.hpp"
+
+void Scene::addPoligonalModel(std::shared_ptr<PoligonalModel> model) 
+{
+    m_poligonalModels.push_back(model);
+}
+
+void Scene::addFlash(std::shared_ptr<Flash> flash) 
+{
+    m_flashs.push_back(flash);
+}
+
+std::shared_ptr<PoligonalModel> getPoligonalModel(int index)
+{
+    return m_poligonalModels[index];
+}
+
+std::shared_ptr<Flash> getFlash(int index)
+{
+    return m_flashs[index];
+
+}
+
+void removePoligonalModel(int index)
+{
+    m_poligonalModels.erase(m_poligonalModels.begin()+index);
+}
+
+void removeFlash(int index)
+{
+    m_flashs.erase(m_flashs.begin()+index);
+}
+
+
